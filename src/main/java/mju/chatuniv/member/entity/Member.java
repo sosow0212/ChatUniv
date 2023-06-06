b921bd53d430402f83a57a5e33f20e4c51eac2e1
sosow0212 <sosow0212@naver.com>
2023-06-06 19:12:59 +0900

    feat: Member Entity 생성
    Co-authored-by: JangAJang <janghee5395@gmail.com>

---
+++ src/main/java/mju/chatuniv/member/entity/Member.java
@@ -0,0 +1,64 @@
+package mju.chatuniv.member.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.regex.Pattern;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Member() {
+    }
+
+    private Member(final Long id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    private Member(final String email, final String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static Member from(final String email, final String password) {
+        if (!validateEmailFormat(email)) {
+            throw new IllegalArgumentException("이메일 형식");
+        }
+
+        if (password.isBlank()) {
+            throw new IllegalArgumentException("패스워드");
+        }
+
+        return new Member(email, password);
+    }
+
+    private static boolean validateEmailFormat(final String email) {
+        return Pattern.matches("^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

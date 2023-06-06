e3e59c3234f3a16e817a6d2999e78702d5f26a92
sosow0212 <sosow0212@naver.com>
2023-06-06 18:00:48 +0900

    feat: Init Project

---
+++ src/main/java/mju/chatuniv/ChatUnivApplication.java
@@ -0,0 +1,13 @@
+package mju.chatuniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatUnivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatUnivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mju/chatuniv/ChatUnivApplicationTests.java
@@ -0,0 +1,13 @@
+package mju.chatuniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatUnivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

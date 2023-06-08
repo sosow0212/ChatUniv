4da3f576f294c5384fba462e7121df199a51aeb3
sosow0212 <sosow0212@naver.com>
2023-06-08 17:05:02 +0900

    fix: email이 중복이 되는 문제 해결

--- src/main/java/mju/chatuniv/member/entity/Member.java
+++ src/main/java/mju/chatuniv/member/entity/Member.java
@@ -17,7 +17,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)

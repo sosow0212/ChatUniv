2bfeeddb9caebb47eded9b6f584e210d8ee49faa
sosow0212 <sosow0212@naver.com>
2023-06-11 23:28:19 +0900

    feat: Auth 기능 관련 예외 세분화

--- src/main/java/mju/chatuniv/member/exception/AuthorizationInvalidException.java
+++
@@ -1,8 +0,0 @@
-package mju.chatuniv.member.exception;
-
-public class AuthorizationInvalidException extends RuntimeException {
-
-    public AuthorizationInvalidException(final String email) {
-        super("Member 정보가 일치하지 않습니다. 요청하신 email : " + email);
-    }
-}

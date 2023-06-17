5fabc79b56165ce8570f73ccc3444a1ccb71c5f8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-17 21:28:45 +0900

    feat: 현재 사용중이 회원의 정보를 조회하는 컨트롤러 로직 구현
    Co-authored-by: sosow0212 <sosow0212@naver.com>

---
+++ src/main/java/mju/chatuniv/member/controller/MemberController.java
@@ -0,0 +1,28 @@
+package mju.chatuniv.member.controller;
+
+import mju.chatuniv.auth.support.JwtLogin;
+import mju.chatuniv.member.application.dto.MemberResponse;
+import mju.chatuniv.member.application.service.MemberService;
+import mju.chatuniv.member.domain.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/members")
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(final MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<MemberResponse> getMemberInfo(@JwtLogin final Member member) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(memberService.getUsingMemberIdAndEmail(member));
+    }
+}

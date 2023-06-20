47e04e85a7ee7061895d345b065a13de53045050
kimtaesoo <kimtaesoo7@naver.com>
2023-06-20 13:16:55 +0900

    feat: 게시판 리포지토리 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>

---
+++ src/main/java/mju/chatuniv/board/domain/BoardRepository.java
@@ -0,0 +1,10 @@
+package mju.chatuniv.board.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Page<Board> findAll(Pageable pageable);
+}

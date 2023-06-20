a8db0a91a15f35ac7f0af73f3a39de90599ca2d8
kimtaesoo <kimtaesoo7@naver.com>
2023-06-20 13:18:01 +0900

    feat: 게시판 관련 예외처리 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>

--- src/main/java/mju/chatuniv/config/ControllerAdvice.java
+++ src/main/java/mju/chatuniv/config/ControllerAdvice.java
@@ -1,8 +1,12 @@
 package mju.chatuniv.config;
 
 import mju.chatuniv.auth.exception.BearerTokenNotFoundException;
+import mju.chatuniv.board.exception.BoardContentBlankException;
+import mju.chatuniv.board.exception.BoardNotFoundException;
+import mju.chatuniv.board.exception.BoardTitleBlankException;
 import mju.chatuniv.member.exception.AuthorizationInvalidException;
 import mju.chatuniv.member.exception.MemberEmailFormatInvalidException;
+import mju.chatuniv.member.exception.MemberNotEqualsException;
 import mju.chatuniv.member.exception.MemberNotFoundException;
 import mju.chatuniv.member.exception.MemberPasswordBlankException;
 import org.springframework.http.HttpStatus;
@@ -36,6 +40,21 @@ public class ControllerAdvice {
         return getBadRequestResponse(exception.getMessage());
     }
 
+    @ExceptionHandler(MemberNotEqualsException.class)
+    public ResponseEntity<String> handlerMemberNotEqualsException(final MemberNotEqualsException exception) {
+        return getBadRequestResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(BoardTitleBlankException.class)
+    public ResponseEntity<String> handlerBoardTitleBlankException(final BoardTitleBlankException exception) {
+        return getBadRequestResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(BoardContentBlankException.class)
+    public ResponseEntity<String> handlerBoardContentBlankException(final BoardContentBlankException exception) {
+        return getBadRequestResponse(exception.getMessage());
+    }
+
     @ExceptionHandler(BearerTokenNotFoundException.class)
     public ResponseEntity<String> handleBearerTokenNotFoundException(final BearerTokenNotFoundException exception) {
         return getNotFoundResponse(exception.getMessage());
@@ -43,6 +62,11 @@ public class ControllerAdvice {
 
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<String> handlerMemberNotFoundException(final MemberNotFoundException exception) {
+        return getNotFoundResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(BoardNotFoundException.class)
+    public ResponseEntity<String> handlerBoardNotFoundException(final BoardNotFoundException exception) {
         return getNotFoundResponse(exception.getMessage());
     }
 

f652df750dec1696be936d8d96444f65219e3e01
kimtaesoo <kimtaesoo7@naver.com>
2023-06-20 17:05:59 +0900

    feat: 페이지의 정보를 한번에 관리하는 DTO 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>

---
+++ src/main/java/mju/chatuniv/board/application/dto/BoardPageInfo.java
@@ -0,0 +1,41 @@
+package mju.chatuniv.board.application.dto;
+
+import mju.chatuniv.board.domain.Board;
+import org.springframework.data.domain.Page;
+
+public class BoardPageInfo {
+
+    private final int totalPage;
+    private final int nowPage;
+    private final int numberOfElements;
+    private final boolean hasNextPage;
+
+    public BoardPageInfo(final int totalPage, final int nowPage,
+                         final int numberOfElements, final boolean hasNextPage) {
+        this.totalPage = totalPage;
+        this.nowPage = nowPage;
+        this.numberOfElements = numberOfElements;
+        this.hasNextPage = hasNextPage;
+    }
+
+    public static BoardPageInfo from(final Page<Board> pageInfo) {
+        return new BoardPageInfo(pageInfo.getTotalPages(), pageInfo.getNumber(),
+            pageInfo.getNumberOfElements(), pageInfo.hasNext());
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public int getNowPage() {
+        return nowPage;
+    }
+
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public boolean isHasNextPage() {
+        return hasNextPage;
+    }
+}

471801f9675b9d51cb409edb5a04c477e26a1af9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-22 21:33:58 +0900

    test: 로그인한 회원의 인조키와 이메일 조회의 통합 테스트 구현

---
+++ src/test/java/mju/chatuniv/member/controller/MemberControllerIntegrationTest.java
@@ -0,0 +1,62 @@
+package mju.chatuniv.member.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import mju.chatuniv.auth.application.AuthService;
+import mju.chatuniv.auth.application.dto.TokenResponse;
+import mju.chatuniv.member.application.dto.MemberCreateRequest;
+import mju.chatuniv.member.application.dto.MemberLoginRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Date;
+
+@Sql(value = "/data.sql")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MemberControllerIntegrationTest {
+
+    private static final String BEARER_ = "Bearer ";
+
+    @Autowired
+    private AuthService authService;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = this.port;
+    }
+
+    @DisplayName("토큰을 통해 현재 회원의 아이디와 이메일을 조회한다.")
+    @Test
+    void get_current_members_id_and_email() {
+        // given
+        authService.register(new MemberCreateRequest("a@a.com", "1234"));
+
+        TokenResponse tokenResponse = authService.login(new MemberLoginRequest("a@a.com", "1234"));
+
+        // when
+        Response response = RestAssured.given()
+                .header("Authorization", BEARER_ + tokenResponse.getAccessToken())
+                .when()
+                .get("/api/members");
+
+        String bodyAsString = response.body().asString();
+
+        // then
+        Assertions.assertAll(() -> {
+            response.then()
+                    .statusCode(HttpStatus.OK.value());
+            Assertions.assertTrue(bodyAsString.contains("memberId"));
+            Assertions.assertTrue(bodyAsString.contains("email"));
+        });
+    }
+}

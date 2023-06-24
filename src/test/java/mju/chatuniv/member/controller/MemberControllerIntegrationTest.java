4b24a39e10bac9f80b4d6f53bf6bb12e137c2b11
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-24 10:50:47 +0900

    test: 현재 회원의 비밀번호 수정 통합 테스트코드 생성

--- src/test/java/mju/chatuniv/member/controller/MemberControllerIntegrationTest.java
+++ src/test/java/mju/chatuniv/member/controller/MemberControllerIntegrationTest.java
@@ -1,9 +1,13 @@
 package mju.chatuniv.member.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import mju.chatuniv.auth.application.AuthService;
 import mju.chatuniv.auth.application.dto.TokenResponse;
+import mju.chatuniv.member.application.dto.ChangePasswordRequest;
 import mju.chatuniv.member.application.dto.MemberCreateRequest;
 import mju.chatuniv.member.application.dto.MemberLoginRequest;
 import org.junit.jupiter.api.Assertions;
@@ -58,5 +62,43 @@ public class MemberControllerIntegrationTest {
             Assertions.assertTrue(bodyAsString.contains("memberId"));
             Assertions.assertTrue(bodyAsString.contains("email"));
         });
+    }
+
+    @DisplayName("토큰을 통해 현재 회원의 아이디와 이메일을 조회한다.")
+    @Test
+    void change_current_members_password() {
+        // given
+        authService.register(new MemberCreateRequest("a@a.com", "1234"));
+
+        TokenResponse tokenResponse = authService.login(new MemberLoginRequest("a@a.com", "1234"));
+
+        ChangePasswordRequest changePasswordRequest =
+                new ChangePasswordRequest("1234", "5678", "5678");
+
+        // when
+        Response response = RestAssured.given()
+                .header("Authorization", BEARER_ + tokenResponse.getAccessToken())
+                .contentType(ContentType.JSON)
+                .body(makeJson(changePasswordRequest))
+                .when()
+                .patch("/api/members");
+
+        String bodyAsString = response.body().asString();
+
+        // then
+        Assertions.assertAll(() -> {
+            response.then()
+                    .statusCode(HttpStatus.OK.value());
+            Assertions.assertTrue(bodyAsString.contains("memberId"));
+            Assertions.assertTrue(bodyAsString.contains("email"));
+        });
+    }
+
+    private String makeJson(Object object) {
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

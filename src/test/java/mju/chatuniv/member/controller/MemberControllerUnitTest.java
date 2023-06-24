a8cc5099c9e8b26f3ad247813eb2c253109eda80
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-24 10:50:40 +0900

    test: 현재 회원의 비밀번호 수정 단위 테스트코드 생성

--- src/test/java/mju/chatuniv/member/controller/MemberControllerUnitTest.java
+++ src/test/java/mju/chatuniv/member/controller/MemberControllerUnitTest.java
@@ -1,13 +1,18 @@
 package mju.chatuniv.member.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import mju.chatuniv.config.ArgumentResolverConfig;
+import mju.chatuniv.member.application.dto.ChangePasswordRequest;
 import mju.chatuniv.member.application.dto.MemberResponse;
 import mju.chatuniv.member.application.service.MemberService;
 import mju.chatuniv.member.domain.Member;
 import mju.chatuniv.member.domain.MemberRepository;
+import mju.chatuniv.member.exception.NewPasswordsNotMatchingException;
+import mju.chatuniv.member.exception.NotCurrentPasswordException;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,7 +25,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.util.Date;
@@ -30,8 +38,7 @@ import static mju.chatuniv.helper.RestDocsHelper.customDocument;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -67,8 +74,7 @@ public class MemberControllerUnitTest {
         given(memberService.getUsingMemberIdAndEmail(any(Member.class))).willReturn(memberResponse);
 
         //expected
-        mockMvc.perform(get("/api/members")
-                .header(HttpHeaders.AUTHORIZATION, BEARER_+ createTokenByMember(member)))
+        createRequestWithToken(get("/api/members"), member, null)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.memberId").value(member.getId()))
                 .andExpect(jsonPath("$.email").value(member.getEmail()))
@@ -94,6 +100,119 @@ public class MemberControllerUnitTest {
         mockMvc.perform(get("/api/members"))
                 .andExpect(status().isUnauthorized())
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("현재 비밀번호, 새 비밀번호, 새 비밀번호 재입력을 성공적으로 입력하면 비밀번호가 교체된다. ")
+    @Test
+    public void change_current_members_password() throws Exception {
+        //given
+        Member member = createMember();
+
+        MemberResponse memberResponse = MemberResponse.from(member);
+
+        ChangePasswordRequest changePasswordRequest =
+                new ChangePasswordRequest("1234", "5678", "5678");
+
+        given(memberService.changeMembersPassword(any(Member.class), any(ChangePasswordRequest.class)))
+                .willReturn(memberResponse);
+
+        //expected
+        createRequestWithToken(patch("/api/members"), member, changePasswordRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.memberId").value(member.getId()))
+                .andExpect(jsonPath("$.email").value(member.getEmail()))
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(customDocument("change_member_password",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("로그인 후 제공되는 Bearer 토큰")
+                        ),
+                        requestFields(
+                                fieldWithPath(".currentPassword").description("기존 비밀번호"),
+                                fieldWithPath(".newPassword").description("새 비밀번호"),
+                                fieldWithPath(".newPasswordCheck").description("새 비밀번호 재입력")
+                        ),
+                        responseFields(
+                                fieldWithPath(".memberId").description("로그인한 MEMBER의 ID"),
+                                fieldWithPath(".email").description("로그인한 MEMBER의 EMAIL")
+                        )
+                ));
+    }
+
+    @DisplayName("토큰이 없을 경우 401에러와 함께 비밀번호 변경을 실패한다. ")
+    @Test
+    public void fail_to_change_password_Unauthorized() throws Exception {
+        //given
+        ChangePasswordRequest changePasswordRequest =
+                new ChangePasswordRequest("1234", "5678", "5678");
+
+        //expected
+        createRequestWithoutToken(patch("/api/members"), changePasswordRequest)
+                .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("입력한 현재 비밀번호가 기존과 다르면 400에러와 메시지를 반환하며 비밀번호 변경을 실패한다.")
+    @Test
+    public void fail_to_change_password_Not_Current_Password() throws Exception {
+        //given
+        Member member = createMember();
+
+        MemberResponse memberResponse = MemberResponse.from(member);
+
+        ChangePasswordRequest changePasswordRequest =
+                new ChangePasswordRequest("5678", "5678", "5678");
+
+        given(memberService.changeMembersPassword(any(Member.class), any(ChangePasswordRequest.class)))
+                .willThrow(NotCurrentPasswordException.class);
+
+        //expected
+        createRequestWithToken(patch("/api/members"), member, changePasswordRequest)
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("입력한 현재 비밀번호가 기존과 다르면 400에러와 메시지를 반환하며 비밀번호 변경을 실패한다.")
+    @Test
+    public void fail_to_change_password_New_Password_Unmatched() throws Exception {
+        //given
+        Member member = createMember();
+
+        MemberResponse memberResponse = MemberResponse.from(member);
+
+        ChangePasswordRequest changePasswordRequest =
+                new ChangePasswordRequest("1234", "5678", "9012");
+
+        given(memberService.changeMembersPassword(any(Member.class), any(ChangePasswordRequest.class)))
+                .willThrow(NewPasswordsNotMatchingException.class);
+
+        //expected
+        createRequestWithToken(patch("/api/members"), member, changePasswordRequest)
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    private ResultActions createRequestWithToken(final MockHttpServletRequestBuilder builder, final Member member, final Object object) throws Exception {
+        return mockMvc.perform(builder
+                .header(HttpHeaders.AUTHORIZATION, BEARER_+ createTokenByMember(member))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(object)));
+    }
+
+    private ResultActions createRequestWithoutToken(final MockHttpServletRequestBuilder builder, final Object object) throws Exception {
+        return mockMvc.perform(builder
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(object)));
+    }
+
+    private String makeJson(Object object) {
+        if(object == null) {
+            return null;
+        }
+
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private String createTokenByMember(Member member) {

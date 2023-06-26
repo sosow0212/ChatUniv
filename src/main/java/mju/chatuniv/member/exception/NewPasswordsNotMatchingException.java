f33dcd1b86d563861ac835d05b31f9204fe71b33
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-26 18:27:11 +0900

    style: 개행 추가

--- src/main/java/mju/chatuniv/member/exception/NewPasswordsNotMatchingException.java
+++ src/main/java/mju/chatuniv/member/exception/NewPasswordsNotMatchingException.java
@@ -1,6 +1,7 @@
 package mju.chatuniv.member.exception;
 
 public class NewPasswordsNotMatchingException extends RuntimeException {
+
     public NewPasswordsNotMatchingException() {
         super("입력하신 새 비밀번호가 서로 일치하지 않습니다.");
     }

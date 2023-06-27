bf274388aa321a7f13876a6fa8b05b4eff89aedc
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-27 20:46:10 +0900

    test: MockTestHelper의 프로퍼티 로드 불가로 사용할 수 없던 JwtTokenProvider의 인스턴스를 ReflectionTestUtils.setField로 초기화하게 테스트코드 추가

--- src/test/java/mju/chatuniv/helper/MockTestHelper.java
+++ src/test/java/mju/chatuniv/helper/MockTestHelper.java
@@ -1,20 +1,23 @@
 package mju.chatuniv.helper;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import mju.chatuniv.auth.infrastructure.JwtTokenProvider;
+import mju.chatuniv.auth.support.JwtLoginResolver;
 import mju.chatuniv.member.domain.Member;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.http.HttpHeaders;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
 import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import java.util.Date;
+import static mju.chatuniv.fixture.member.MemberFixture.createMember;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 public class MockTestHelper {
 
@@ -22,14 +25,28 @@ public class MockTestHelper {
 
     private final MockMvc mockMvc;
 
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
+    @Mock
+    private JwtLoginResolver jwtLoginResolver;
 
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public MockTestHelper(final MockMvc mockMvc) {
         this.mockMvc = mockMvc;
+        this.jwtTokenProvider = new JwtTokenProvider();
+        ReflectionTestUtils.setField(jwtTokenProvider,
+                "secretKey",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno");
+        ReflectionTestUtils.setField(jwtTokenProvider,
+                "validityInMilliseconds",
+                3600000);
+    }
+
+    @BeforeEach
+    void init() throws Exception {
+        Member member = createMember();
+        given(jwtLoginResolver.supportsParameter(any())).willReturn(true);
+        given(jwtLoginResolver.resolveArgument(any(), any(), any(), any())).willReturn(member);
+        given(jwtTokenProvider.createAccessToken(member.getEmail())).willReturn("Bearer " + any(String.class));
     }
 
     public ResultActions createMockRequestWithTokenAndWithoutContent(final MockHttpServletRequestBuilder uriBuilder, final Member member) throws Exception {
@@ -45,19 +62,18 @@ public class MockTestHelper {
                 .content(makeJson(object)));
     }
 
+    public ResultActions createMockRequestWithoutTokenAndWithContent(final MockHttpServletRequestBuilder uriBuilder, final Object object) throws Exception {
+        return mockMvc.perform(uriBuilder
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(object)));
+    }
+
+    public ResultActions createMockRequestWithoutTokenAndContent(final MockHttpServletRequestBuilder uriBuilder) throws Exception {
+        return mockMvc.perform(uriBuilder);
+    }
+
     private String createTokenByMember(final Member member) {
-        Claims claims = Jwts.claims()
-                .setSubject(member.getEmail());
-
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
+        return jwtTokenProvider.createAccessToken(member.getEmail());
     }
 
     private String makeJson(final Object object) {

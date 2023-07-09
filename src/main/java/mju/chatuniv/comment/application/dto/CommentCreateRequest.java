ac7eda0fcd59594ce369ffc883386b3d7fe1f8e7
eom-tae-in <eti0728@daum.net>
2023-07-09 22:53:21 +0900

    feat: CommentCreateRequest Dto 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/main/java/mju/chatuniv/comment/application/dto/CommentCreateRequest.java
+++ src/main/java/mju/chatuniv/comment/application/dto/CommentCreateRequest.java
@@ -1,17 +1,30 @@
 package mju.chatuniv.comment.application.dto;
 
+import mju.chatuniv.comment.domain.CommentType;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CommentCreateRequest {
 
+    @NotNull
+    private CommentType commentType;
+    @NotBlank
     private String content;
 
     private CommentCreateRequest() {
     }
 
-    public CommentCreateRequest(String content) {
+    public CommentCreateRequest(final CommentType commentType, final String content) {
+        this.commentType = commentType;
         this.content = content;
     }
 
     public String getContent() {
         return content;
+    }
+
+    public CommentType getCommentType() {
+        return commentType;
     }
 }

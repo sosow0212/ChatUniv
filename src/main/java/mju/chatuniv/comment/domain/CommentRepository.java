d1e6c82dc598b58599c194a07879983704eb4cea
eom-tae-in <eti0728@daum.net>
2023-07-09 22:53:36 +0900

    feat: CommentRepository 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/main/java/mju/chatuniv/comment/domain/CommentRepository.java
+++ src/main/java/mju/chatuniv/comment/domain/CommentRepository.java
@@ -4,8 +4,13 @@ import mju.chatuniv.board.domain.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<BoardComment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<BoardComment> findAllByBoard(final Pageable pageable, final Board board);
+
+    @Query("select c from Comment c join BoardComment bc on c.id = bc.id where bc.board.id = :boardId")
+    Page<BoardComment> findAllByBoard(final Pageable pageable, @Param("board") final Board board, @Param("boardId") Long boardId);
+
 }

ff9582f23a5f42d2d465a3e350b1ec8a63ea78b0
eom-tae-in <eti0728@daum.net>
2023-07-09 23:09:16 +0900

    feat: BoardCommentController 클래스 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

---
+++ src/main/java/mju/chatuniv/comment/controller/BoardCommentController.java
@@ -0,0 +1,42 @@
+package mju.chatuniv.comment.controller;
+
+import mju.chatuniv.auth.support.JwtLogin;
+import mju.chatuniv.board.domain.Board;
+import mju.chatuniv.comment.application.dto.CommentAllResponse;
+import mju.chatuniv.comment.application.dto.CommentCreateRequest;
+import mju.chatuniv.comment.application.dto.CommentResponse;
+import mju.chatuniv.comment.application.service.BoardCommentService;
+import mju.chatuniv.member.domain.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BoardCommentController {
+
+    private final BoardCommentService boardCommentService;
+
+    public BoardCommentController(final BoardCommentService boardCommentService) {
+        this.boardCommentService = boardCommentService;
+    }
+
+    @PostMapping("/boards/{boardId}/comments")
+    public ResponseEntity<CommentResponse> createBoardComment(@PathVariable("boardId") final Long boardId, @JwtLogin final Member member, @RequestBody final CommentCreateRequest commentCreateRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(boardCommentService.createBoardComment(boardId, member, commentCreateRequest));
+    }
+
+    @GetMapping("/boards/{boardId}/comments")
+    public ResponseEntity<CommentAllResponse> findCommentsByBoard(@PathVariable("boardId") final Long boardId, @PageableDefault final Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(boardCommentService.findCommentsByBoard(boardId, pageable));
+    }
+}

b4aa7c043c3f35742f75ddddd3810487b0ee20be
eom-tae-in <eti0728@daum.net>
2023-07-16 00:01:10 +0900

    feat: CommentNotFoundException 클래스 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/test/java/mju/chatuniv/fixture/member/MemberFixture.java
+++ src/test/java/mju/chatuniv/fixture/member/MemberFixture.java
@@ -7,4 +7,8 @@ public class MemberFixture {
     public static Member createMember() {
         return Member.from(1L, "a@a.com", "1234");
     }
+
+    public static Member otherMember() {
+        return Member.from(2L, "b@b.com", "5678");
+    }
 }

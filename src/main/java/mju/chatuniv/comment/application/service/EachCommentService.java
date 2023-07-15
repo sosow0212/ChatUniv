43380d35999ab6ffb8923bfbcf37cd01bbd481e9
eom-tae-in <eti0728@daum.net>
2023-07-15 19:44:34 +0900

    feat: Comment의 생성 메서드와 조회 메서드를 추상화하는 EachCommentService 인터페이스 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

---
+++ src/main/java/mju/chatuniv/comment/application/service/EachCommentService.java
@@ -0,0 +1,14 @@
+package mju.chatuniv.comment.application.service;
+
+import mju.chatuniv.comment.application.dto.CommentAllResponse;
+import mju.chatuniv.comment.application.dto.CommentRequest;
+import mju.chatuniv.comment.application.dto.CommentResponse;
+import mju.chatuniv.member.domain.Member;
+import org.springframework.data.domain.Pageable;
+
+public interface EachCommentService {
+
+    CommentResponse create(Long boardId, Member member, CommentRequest commentRequest);
+
+    CommentAllResponse findComments(Long boardId, Pageable pageable);
+}

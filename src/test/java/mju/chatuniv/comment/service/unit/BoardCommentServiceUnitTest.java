52a38afede7891cea561bc3e29c18424a6a98e1e
eom-tae-in <eti0728@daum.net>
2023-07-16 00:02:39 +0900

    test: BoardCommentServiceUnitTest 클래스 생성
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

---
+++ src/test/java/mju/chatuniv/comment/service/unit/BoardCommentServiceUnitTest.java
@@ -0,0 +1,82 @@
+package mju.chatuniv.comment.service.unit;
+
+import mju.chatuniv.board.domain.Board;
+import mju.chatuniv.board.domain.BoardRepository;
+import mju.chatuniv.board.exception.exceptions.BoardNotFoundException;
+import mju.chatuniv.comment.application.dto.CommentRequest;
+import mju.chatuniv.comment.application.service.BoardCommentService;
+import mju.chatuniv.comment.exception.exceptions.CommentContentBlankException;
+import mju.chatuniv.fixture.board.BoardFixture;
+import mju.chatuniv.fixture.member.MemberFixture;
+import mju.chatuniv.member.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardCommentServiceUnitTest {
+
+    private Member member;
+    private Board board;
+
+    @InjectMocks
+    private BoardCommentService boardCommentService;
+
+    @Mock
+    private BoardRepository boardRepository;
+
+    @BeforeEach
+    void init() {
+        member = MemberFixture.createMember();
+        board = BoardFixture.createBoard(member);
+    }
+
+    @DisplayName("댓글 생성시 게시판 아이디가 존재하지 않는다면 예외를 발생한다.")
+    @Test
+    void throws_exception_when_create_comment_with_invalid_board_id() {
+        //given
+        Long wrongBoardId = 2L;
+        CommentRequest commentRequest = new CommentRequest("content");
+
+        given(boardRepository.findById(wrongBoardId)).willThrow(BoardNotFoundException.class);
+
+        //when & then
+        assertThatThrownBy(() -> boardCommentService.create(wrongBoardId, member, commentRequest))
+                .isInstanceOf(BoardNotFoundException.class);
+    }
+
+    @DisplayName("댓글 생성시 내용이 비어있으면 예외가 발생한다.")
+    @ParameterizedTest(name = "{index} : {0}")
+    @MethodSource("commentRequestProvider")
+    void throws_exception_when_create_comment_with_blank(final String text, final CommentRequest commentRequest) {
+        //given
+        Long boardId = 2L;
+
+        given(boardRepository.findById(boardId)).willReturn(Optional.of(board));
+
+        //when & then
+        assertThatThrownBy(() -> boardCommentService.create(boardId, member, commentRequest))
+                .isInstanceOf(CommentContentBlankException.class);
+    }
+
+    private static Stream<Arguments> commentRequestProvider() {
+        return Stream.of(
+                Arguments.of("내용이 null인 경우", new CommentRequest(null)),
+                Arguments.of("내용이 공백인 경우", new CommentRequest("")),
+                Arguments.of("내용이 빈 칸인 경우", new CommentRequest(" "))
+        );
+    }
+}

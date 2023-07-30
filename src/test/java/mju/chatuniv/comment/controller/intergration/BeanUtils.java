ecfd1c3a3b5c59e3b8e1d8ba5c55c4715f27684f
eom-tae-in <eti0728@daum.net>
2023-07-30 18:09:28 +0900

    refactor: 메서드 네이밍 변경
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/test/java/mju/chatuniv/comment/controller/intergration/BeanUtils.java
+++ src/test/java/mju/chatuniv/comment/controller/intergration/BeanUtils.java
@@ -1,6 +1,6 @@
 package mju.chatuniv.comment.controller.intergration;
 
-import mju.chatuniv.comment.application.service.EachCommentService;
+import mju.chatuniv.comment.application.service.CommentService;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
@@ -12,8 +12,8 @@ public class BeanUtils {
 
     private static final ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
 
-    public static List<EachCommentService> getBeansOfType() {
-        Map<String, EachCommentService> beansOfType = new TreeMap<>(applicationContext.getBeansOfType(EachCommentService.class));
+    public static List<CommentService> getBeansOfCommentServiceType() {
+        Map<String, CommentService> beansOfType = new TreeMap<>(applicationContext.getBeansOfType(CommentService.class));
         return new ArrayList<>(beansOfType.values());
     }
 }

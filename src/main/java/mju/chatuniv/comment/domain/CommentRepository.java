dec61a01717a827b0918a06dd82ee7eba445a30b
eom-tae-in <eti0728@daum.net>
2023-07-30 18:15:03 +0900

    refactor: 메서드명 변경
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/main/java/mju/chatuniv/comment/domain/CommentRepository.java
+++ src/main/java/mju/chatuniv/comment/domain/CommentRepository.java
@@ -1,6 +1,5 @@
 package mju.chatuniv.comment.domain;
 
-import mju.chatuniv.board.domain.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +9,5 @@ import org.springframework.data.repository.query.Param;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join BoardComment bc on c.id = bc.id where bc.board.id = :boardId")
-    Page<Comment> findAllByBoard(final Pageable pageable, @Param("board") final Board board, @Param("boardId") final Long boardId);
+    Page<Comment> findAllByBoardId(Pageable pageable, @Param("boardId") Long boardId);
 }

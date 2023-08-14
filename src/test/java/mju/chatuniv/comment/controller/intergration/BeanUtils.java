a559323bae3688519702413a794c36578fddca27
eom-tae-in <eti0728@daum.net>
2023-08-14 15:06:19 +0900

    refactor: Map 구현제 TreeMap에서 HashMap으로 변경
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/test/java/mju/chatuniv/comment/controller/intergration/BeanUtils.java
+++ src/test/java/mju/chatuniv/comment/controller/intergration/BeanUtils.java
@@ -4,16 +4,16 @@ import mju.chatuniv.comment.application.service.CommentService;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class BeanUtils {
 
     private static final ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
 
     public static List<CommentService> getBeansOfCommentServiceType() {
-        Map<String, CommentService> beansOfType = new TreeMap<>(applicationContext.getBeansOfType(CommentService.class));
+        Map<String, CommentService> beansOfType = new HashMap<>(applicationContext.getBeansOfType(CommentService.class));
         return new ArrayList<>(beansOfType.values());
     }
 }

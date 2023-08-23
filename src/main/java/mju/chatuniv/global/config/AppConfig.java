eae775361687db3d30e9d57b194dd5c5c430a933
eom-tae-in <eti0728@daum.net>
2023-08-23 18:31:07 +0900

    feat: QueryDsl 추가
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>
    
    Co-authored-by: JangAJang <janghee5395@gmail.com>

---
+++ src/main/java/mju/chatuniv/global/config/AppConfig.java
@@ -0,0 +1,22 @@
+package mju.chatuniv.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class AppConfig {
+
+    private final EntityManager entityManager;
+
+    public AppConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}

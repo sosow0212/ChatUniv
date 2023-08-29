2ed50448224d0a2d524fac959719f2afeb9f618a
eom-tae-in <eti0728@daum.net>
2023-08-29 19:19:51 +0900

    refactor: Thread-safe를 위해 @PersistenceContext 어노테이션 추가
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/main/java/mju/chatuniv/global/config/AppConfig.java
+++ src/main/java/mju/chatuniv/global/config/AppConfig.java
@@ -1,19 +1,16 @@
 package mju.chatuniv.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class AppConfig {
 
-    private final EntityManager entityManager;
-
-    public AppConfig(final EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory queryFactory() {

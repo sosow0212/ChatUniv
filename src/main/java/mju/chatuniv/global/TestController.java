26dacc10d8a923460f72c0bdf7500eeeea6cc06b
sosow0212 <sosow0212@naver.com>
2023-09-18 14:01:07 +0900

    feat: TestController 추가

---
+++ src/main/java/mju/chatuniv/global/TestController.java
@@ -0,0 +1,13 @@
+package mju.chatuniv.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}

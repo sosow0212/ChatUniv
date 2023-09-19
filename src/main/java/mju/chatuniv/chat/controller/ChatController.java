0a891992634ffefee6b651202c31c37ccd0b0bd1
sosow0212 <sosow0212@naver.com>
2023-09-19 21:20:07 +0900

    refactor: 자신의 채팅방에서만 질문 남길 수 있도록 변경

--- src/main/java/mju/chatuniv/chat/controller/ChatController.java
+++ src/main/java/mju/chatuniv/chat/controller/ChatController.java
@@ -1,11 +1,11 @@
 package mju.chatuniv.chat.controller;
 
 import mju.chatuniv.auth.support.JwtLogin;
+import mju.chatuniv.chat.controller.dto.ConversationResponse;
+import mju.chatuniv.chat.domain.chat.Conversation;
 import mju.chatuniv.chat.service.ChatService;
 import mju.chatuniv.chat.service.dto.chat.ChatPromptRequest;
 import mju.chatuniv.chat.service.dto.chat.ChattingHistoryResponse;
-import mju.chatuniv.chat.domain.chat.Conversation;
-import mju.chatuniv.chat.controller.dto.ConversationResponse;
 import mju.chatuniv.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,15 +41,17 @@ public class ChatController {
 
     @PostMapping("/{chatId}/mild")
     public ResponseEntity<ConversationResponse> useMildChatBot(@RequestBody final ChatPromptRequest prompt,
-                                                                @PathVariable final Long chatId) {
-        Conversation conversation = chatService.useChatBot(prompt.getPrompt(), chatId, true);
+                                                               @PathVariable final Long chatId,
+                                                               @JwtLogin final Member member) {
+        Conversation conversation = chatService.useChatBot(prompt.getPrompt(), chatId, true, member);
         return ResponseEntity.ok(ConversationResponse.from(conversation));
     }
 
     @PostMapping("/{chatId}/raw")
     public ResponseEntity<ConversationResponse> useFunnyChatBot(@RequestBody final ChatPromptRequest prompt,
-                                                                @PathVariable final Long chatId) {
-        Conversation conversation = chatService.useChatBot(prompt.getPrompt(), chatId, false);
+                                                                @PathVariable final Long chatId,
+                                                                @JwtLogin final Member member) {
+        Conversation conversation = chatService.useChatBot(prompt.getPrompt(), chatId, false, member);
         return ResponseEntity.ok(ConversationResponse.from(conversation));
     }
 }

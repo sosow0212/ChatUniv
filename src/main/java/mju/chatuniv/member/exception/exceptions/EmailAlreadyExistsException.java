3afbe31ce075076a79ee69e1906049b106b38e1b
kimtaesoo <kimtaesoo7@naver.com>
2023-09-21 23:23:45 +0900

    feat: 이메일 중복 예외 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>

---
+++ src/main/java/mju/chatuniv/member/exception/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package mju.chatuniv.member.exception.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException{
+
+    public EmailAlreadyExistsException(final String password) {
+        super("이미 존재하는 이메일입니다. : " + password);
+    }
+}

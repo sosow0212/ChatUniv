72a172599ff6ea16f1007251c172bb94127210ba
kimtaesoo <kimtaesoo7@naver.com>
2023-09-21 23:24:55 +0900

    test: auth 예외 처리에 대한 테스트 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>

--- src/test/java/mju/chatuniv/auth/controller/AuthControllerUnitTest.java
+++ src/test/java/mju/chatuniv/auth/controller/AuthControllerUnitTest.java
@@ -1,22 +1,5 @@
 package mju.chatuniv.auth.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import mju.chatuniv.auth.service.JwtAuthService;
-import mju.chatuniv.auth.controller.dto.TokenResponse;
-import mju.chatuniv.member.service.dto.MemberCreateRequest;
-import mju.chatuniv.member.service.dto.MemberLoginRequest;
-import mju.chatuniv.member.domain.Member;
-import mju.chatuniv.member.controller.dto.MemberResponse;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static mju.chatuniv.fixture.member.MemberFixture.createMember;
 import static mju.chatuniv.helper.RestDocsHelper.customDocument;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +10,27 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mju.chatuniv.auth.controller.dto.TokenResponse;
+import mju.chatuniv.auth.service.JwtAuthService;
+import mju.chatuniv.member.controller.dto.MemberResponse;
+import mju.chatuniv.member.domain.Member;
+import mju.chatuniv.member.exception.exceptions.AuthorizationInvalidPasswordException;
+import mju.chatuniv.member.exception.exceptions.EmailAlreadyExistsException;
+import mju.chatuniv.member.exception.exceptions.MemberEmailFormatInvalidException;
+import mju.chatuniv.member.exception.exceptions.MemberNotFoundException;
+import mju.chatuniv.member.service.dto.MemberCreateRequest;
+import mju.chatuniv.member.service.dto.MemberLoginRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AuthController.class)
 @AutoConfigureRestDocs
@@ -94,5 +98,130 @@ public class AuthControllerUnitTest {
                                 fieldWithPath(".accessToken").description("Jwt AccessToken 값")
                         )
                 ));
+    }
+
+    @DisplayName("이미 존재하는 이메일로 회원가입 시도시 예외발생.")
+    @Test
+    void fail_to_sign_up_with_duplicated_email() throws Exception {
+        // given
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("a@a.com", "1234");
+
+        given(jwtAuthService.register(Mockito.any(MemberCreateRequest.class))).willThrow(
+                new EmailAlreadyExistsException(memberCreateRequest.getEmail()));
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateRequest))
+                ).andExpect(status().isForbidden())
+                .andDo(customDocument("fail_to_register_member_with_duplicated_email",
+                        requestFields(
+                                fieldWithPath(".email").description("회원가입할 이메일 주소"),
+                                fieldWithPath(".password").description("사용할 패스워드")
+                        )
+                ));
+    }
+
+    @DisplayName("잘못된 이메일 형식으로 회원가입 시도시 예외발생.")
+    @Test
+    void fail_to_sign_up_wrong_email() throws Exception {
+        // given
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("a@.com", "1234");
+
+        given(jwtAuthService.register(Mockito.any(MemberCreateRequest.class))).willThrow(
+                new MemberEmailFormatInvalidException(memberCreateRequest.getEmail()));
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateRequest))
+                ).andExpect(status().isBadRequest())
+                .andDo(customDocument("fail_to_sign_up_wrong_email",
+                        requestFields(
+                                fieldWithPath(".email").description("회원가입할 이메일 주소"),
+                                fieldWithPath(".password").description("사용할 패스워드")
+                        )
+                ));
+    }
+
+    @DisplayName("비어있는 이메일로 회원가입 시도시 예외발생.")
+    @Test
+    void fail_to_sign_up_empty_email() throws Exception {
+        // given
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("", "1234");
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateRequest))
+                ).andExpect(status().isBadRequest())
+                .andDo(customDocument("fail_to_sign_up_empty_email",
+                        requestFields(
+                                fieldWithPath(".email").description("회원가입할 이메일 주소"),
+                                fieldWithPath(".password").description("사용할 패스워드")
+                        )
+                ));
+    }
+
+    @DisplayName("비어있는 비밀번호 형식으로 회원가입 시도시 예외발생.")
+    @Test
+    void fail_to_sign_up_empty_password() throws Exception {
+        // given
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("as21d@as.com", "");
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateRequest))
+                ).andExpect(status().isOk())
+                .andDo(customDocument("fail_to_sign_up_empty_password",
+                        requestFields(
+                                fieldWithPath(".email").description("회원가입할 이메일 주소"),
+                                fieldWithPath(".password").description("사용할 패스워드")
+                        )
+                ));
+    }
+
+    @DisplayName("잘못된 이메일로 로그인 진행시 예외발생")
+    @Test
+    void fail_to_login_with_not_exist_email() throws Exception {
+        // given
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("a@a.com", "1234");
+
+        given(jwtAuthService.login(any(MemberLoginRequest.class))).willThrow(new MemberNotFoundException());
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberLoginRequest))
+                ).andExpect(status().isNotFound())
+                .andDo(customDocument("fail_to_with_not_exist_login",
+                        requestFields(
+                                fieldWithPath(".email").description("로그인 이메일 주소"),
+                                fieldWithPath(".password").description("로그인 패스워드")
+                        )
+                )).andReturn();
+    }
+
+    @DisplayName("잘못된 비밀번호로 로그인 시도시 예외발생")
+    @Test
+    void fail_to_login_with_wrong_password() throws Exception {
+        // given
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("a@a.com", "1234");
+
+        given(jwtAuthService.login(any(MemberLoginRequest.class))).willThrow(
+                new AuthorizationInvalidPasswordException("1234"));
+
+        // when & then
+        mockMvc.perform(post("/api/auth/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberLoginRequest))
+                ).andExpect(status().isForbidden())
+                .andDo(customDocument("fail_to_with_wrong_password_login",
+                        requestFields(
+                                fieldWithPath(".email").description("로그인 이메일 주소"),
+                                fieldWithPath(".password").description("로그인 패스워드")
+                        )
+                )).andReturn();
     }
 }

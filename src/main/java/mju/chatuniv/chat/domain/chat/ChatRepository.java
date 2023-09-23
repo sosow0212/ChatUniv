32c714f795410529c9a0ce6c8377e773f67c4d79
JangAJang <janghee5395@gmail.com>
2023-09-23 16:00:00 +0900

    feat: Member.id로 chat을 리스트로 반환하는 기능 구현

--- src/main/java/mju/chatuniv/chat/domain/chat/ChatRepository.java
+++ src/main/java/mju/chatuniv/chat/domain/chat/ChatRepository.java
@@ -2,5 +2,9 @@ package mju.chatuniv.chat.domain.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+    List<Chat> findAllByMember_Id(final Long memberId);
 }

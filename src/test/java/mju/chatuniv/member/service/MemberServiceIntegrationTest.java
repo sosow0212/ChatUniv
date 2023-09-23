0f758431ec3766c7c316f7b4d6d062320755cd20
JangAJang <janghee5395@gmail.com>
2023-09-23 16:26:49 +0900

    test: 회원의 채팅방들을 조회하는 기능의 비즈니스 로직 통합 테스트 구현

--- src/test/java/mju/chatuniv/member/service/MemberServiceIntegrationTest.java
+++ src/test/java/mju/chatuniv/member/service/MemberServiceIntegrationTest.java
@@ -1,13 +1,20 @@
 package mju.chatuniv.member.service;
 
+import mju.chatuniv.chat.domain.chat.Chat;
+import mju.chatuniv.chat.domain.chat.ChatRepository;
 import mju.chatuniv.helper.integration.IntegrationTest;
+import mju.chatuniv.member.domain.MemberRepository;
 import mju.chatuniv.member.service.dto.ChangePasswordRequest;
 import mju.chatuniv.member.service.service.MemberService;
 import mju.chatuniv.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static mju.chatuniv.fixture.member.MemberFixture.createMember;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -17,6 +24,12 @@ public class MemberServiceIntegrationTest extends IntegrationTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @DisplayName("회원 정보는 입력받은 회원으로 만든다.")
     @CsvSource({"1, a@a.com, true", "2, b@b.com, false"})
@@ -49,5 +62,21 @@ public class MemberServiceIntegrationTest extends IntegrationTest {
         //then
         memberService.changeMembersPassword(member, changePasswordRequest);
         assertEquals(newPassword, member.getPassword());
+    }
+
+    @DisplayName("회원의 채팅방들을 반환한다.")
+    @Test
+    public void find_members_chat_rooms() {
+        //given
+        Member member = createMember();
+        memberRepository.save(member);
+        IntStream.range(0, 10)
+                .forEach(id -> chatRepository.save(Chat.createDefault(member)));
+
+        //when
+        List<Chat> membersChat = memberService.findMembersChat(member);
+
+        //then
+        assertEquals(membersChat.size(), 10);
     }
 }

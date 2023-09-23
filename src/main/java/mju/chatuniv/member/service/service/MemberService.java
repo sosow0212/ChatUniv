45fc72f46f78502d77f0d861172602c7a4d9d674
JangAJang <janghee5395@gmail.com>
2023-09-23 16:00:21 +0900

    feat: Member.id로 Chat을 List로 반환하는 비즈니스 로직 구현

--- src/main/java/mju/chatuniv/member/service/service/MemberService.java
+++ src/main/java/mju/chatuniv/member/service/service/MemberService.java
@@ -1,5 +1,7 @@
 package mju.chatuniv.member.service.service;
 
+import mju.chatuniv.chat.domain.chat.Chat;
+import mju.chatuniv.chat.domain.chat.ChatRepository;
 import mju.chatuniv.member.service.dto.ChangePasswordRequest;
 import mju.chatuniv.member.domain.Member;
 import mju.chatuniv.member.domain.MemberRepository;
@@ -8,18 +10,27 @@ import mju.chatuniv.member.exception.exceptions.NotCurrentPasswordException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final ChatRepository chatRepository;
 
-    public MemberService(final MemberRepository memberRepository) {
+    public MemberService(final MemberRepository memberRepository, final ChatRepository chatRepository) {
         this.memberRepository = memberRepository;
+        this.chatRepository = chatRepository;
     }
 
     @Transactional(readOnly = true)
     public Member getUsingMemberIdAndEmail(final Member member){
         return member;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Chat> findMembersChat(final Member member) {
+        return chatRepository.findAllByMember_Id(member.getId());
     }
 
     @Transactional

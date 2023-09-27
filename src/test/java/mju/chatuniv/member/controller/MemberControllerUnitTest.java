033077668b35e26f3a4bcb6ad3cb0894f39a8130
eom-tae-in <eti0728@daum.net>
2023-09-27 15:02:51 +0900

    test: 코드 충돌로 인한 코드 수정
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/test/java/mju/chatuniv/member/controller/MemberControllerUnitTest.java
+++ src/test/java/mju/chatuniv/member/controller/MemberControllerUnitTest.java
@@ -1,5 +1,6 @@
 package mju.chatuniv.member.controller;
 
+import static mju.chatuniv.fixture.member.MemberFixture.createMember;
 import static mju.chatuniv.helper.RestDocsHelper.customDocument;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -13,6 +14,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import mju.chatuniv.board.controller.dto.BoardResponse;
+import mju.chatuniv.board.domain.Board;
+import mju.chatuniv.chat.domain.chat.Chat;
+import mju.chatuniv.comment.domain.dto.MembersCommentResponse;
 import mju.chatuniv.global.config.ArgumentResolverConfig;
 import mju.chatuniv.helper.MockTestHelper;
 import mju.chatuniv.member.domain.Member;
@@ -249,6 +257,120 @@ class MemberControllerUnitTest {
                                 fieldWithPath("newPassword").description("새 비밀번호"),
                                 fieldWithPath("newPasswordCheck").description("새 비밀번호 재입력")
                         ))).andReturn();
+    }
+
+    @DisplayName("회원의 채팅방 내역들을 조회하면 현재 회원이 사용한 채팅방이 리스트로 반환된다.")
+    @Test
+    public void find_current_members_chat_rooms() throws Exception {
+        //given
+        given(memberService.findMembersChat(any(Member.class)))
+                .willReturn(makeDummyChats());
+
+        //when & then
+        mockTestHelper.createMockRequestWithTokenAndWithoutContent(get("/api/members/me/chats"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.myChats").isArray())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("회원의 게시물을 조회하면 게시물의 id, 제목, 내용이 반환된다. ")
+    @Test
+    public void find_current_members_boards() throws Exception {
+        //given
+        given(memberService.findMembersBoard(any(Member.class))).willReturn(makeDummyBoards());
+
+        //when&then
+        mockTestHelper.createMockRequestWithTokenAndWithoutContent(get("/api/members/me/boards"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.boardResponses").isArray())
+                .andExpect(jsonPath("$.boardResponses.length()").value(10))
+                .andExpect(jsonPath("$.boardResponses[0].title").value("title0"))
+                .andExpect(jsonPath("$.boardResponses[0].content").value("content0"))
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(customDocument("find_members_boards",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("로그인 후 제공되는 Bearer 토큰")
+                        ),
+                        responseFields(
+                                fieldWithPath(".boardResponses").description("조회시 반환되는 데이터 배열"),
+                                fieldWithPath(".boardResponses[0].boardId").description("조회시 반환되는 board의 id"),
+                                fieldWithPath(".boardResponses[0].title").description("조회시 반환되는 board의 id"),
+                                fieldWithPath(".boardResponses[0].content").description("조회시 반환되는 board의 id")
+                        )
+                ));
+    }
+
+    @DisplayName("토큰이 없을 때 게시물을 조회하면 401에러와 토큰이 없음이 반환된다. ")
+    @Test
+    public void fail_to_find_current_members_boards_Unauthorized() throws Exception {
+        //given
+        given(memberService.findMembersBoard(any(Member.class))).willReturn(makeDummyBoards());
+
+        //when&then
+        mockTestHelper.createMockRequestWithoutTokenAndContent(get("/api/members/me/boards"))
+                .andExpect(status().isUnauthorized())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("회원의 댓글을 조회하면 댓글의 내용, 게시물id, 회원의 이메일이 반환된다. ")
+    @Test
+    public void find_current_members_comments() throws Exception {
+        //given
+        given(memberService.findMembersComment(any(Member.class))).willReturn(makeDummyComments());
+
+        //when&then
+        mockTestHelper.createMockRequestWithTokenAndWithoutContent(get("/api/members/me/comments"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.membersCommentResponses").isArray())
+                .andExpect(jsonPath("$.membersCommentResponses.length()").value(10))
+                .andExpect(jsonPath("$.membersCommentResponses[0].boardId").value(1))
+                .andExpect(jsonPath("$.membersCommentResponses[0].content").value("content0"))
+                .andExpect(jsonPath("$.membersCommentResponses[0].email").value("a@a.com"))
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(customDocument("find_members_comments",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("로그인 후 제공되는 Bearer 토큰")
+                        ),
+                        responseFields(
+                                fieldWithPath(".membersCommentResponses").description("조회시 반환되는 데이터 배열"),
+                                fieldWithPath(".membersCommentResponses[0].email").description("조회시 반환되는 회원의 이메일"),
+                                fieldWithPath(".membersCommentResponses[0].boardId").description(
+                                        "조회시 반환되는 게시물의 id(onclick이벤트를 위해 반환)"),
+                                fieldWithPath(".membersCommentResponses[0].content").description("조회시 반환되는 댓글 내용")
+                        )
+                ));
+    }
+
+    @DisplayName("토큰이 없을 때 댓글을 조회하면 401에러와 토큰이 없음이 반환된다. ")
+    @Test
+    public void fail_to_find_current_members_comments_Unauthorized() throws Exception {
+        //given
+        given(memberService.findMembersComment(any(Member.class))).willReturn(makeDummyComments());
+
+        //when&then
+        mockTestHelper.createMockRequestWithoutTokenAndContent(get("/api/members/me/comments"))
+                .andExpect(status().isUnauthorized())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    private List<Chat> makeDummyChats() {
+        return IntStream.range(0, 10)
+                .mapToObj(each -> Chat.createDefault(Member.of("a@a.com", "1234")))
+                .collect(Collectors.toList());
+    }
+
+    private List<BoardResponse> makeDummyBoards() {
+        return IntStream.range(0, 10)
+                .mapToObj(each -> Board.of("title" + each, "content" + each, createMember()))
+                .map(BoardResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    private List<MembersCommentResponse> makeDummyComments() {
+        return IntStream.range(0, 10)
+                .mapToObj(each -> MembersCommentResponse.of(createMember().getEmail(), (long) (each + 1),
+                        "content" + each))
+                .collect(Collectors.toList());
     }
 }
 
--- src/test/java/mju/chatuniv/member/service/MemberServiceIntegrationTest.java
+++ src/test/java/mju/chatuniv/member/service/MemberServiceIntegrationTest.java
@@ -1,21 +1,48 @@
 package mju.chatuniv.member.service;
 
+import static mju.chatuniv.fixture.member.MemberFixture.createMember;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import mju.chatuniv.board.controller.dto.BoardResponse;
+import mju.chatuniv.board.domain.Board;
+import mju.chatuniv.board.domain.BoardRepository;
+import mju.chatuniv.chat.domain.chat.Chat;
+import mju.chatuniv.chat.domain.chat.ChatRepository;
+import mju.chatuniv.comment.domain.BoardComment;
+import mju.chatuniv.comment.domain.CommentRepository;
+import mju.chatuniv.comment.domain.dto.MembersCommentResponse;
 import mju.chatuniv.helper.integration.IntegrationTest;
 import mju.chatuniv.member.domain.Member;
+import mju.chatuniv.member.domain.MemberRepository;
 import mju.chatuniv.member.service.dto.ChangePasswordRequest;
 import mju.chatuniv.member.service.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class MemberServiceIntegrationTest extends IntegrationTest {
+class MemberServiceIntegrationTest extends IntegrationTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
 
     @DisplayName("회원 정보는 입력받은 회원으로 만든다.")
     @CsvSource({"a@a.com, true", "b@b.com, false"})
@@ -44,5 +71,72 @@ public class MemberServiceIntegrationTest extends IntegrationTest {
         //then
         memberService.changeMembersPassword(member, changePasswordRequest);
         assertEquals(newPassword, member.getPassword());
+    }
+
+    @DisplayName("회원의 채팅방들을 반환한다.")
+    @Test
+    public void find_members_chat_rooms() {
+        //given
+        Member member = initializeMember();
+        IntStream.range(0, 10)
+                .forEach(id -> chatRepository.save(Chat.createDefault(member)));
+
+        //when
+        List<Chat> membersChat = memberService.findMembersChat(member);
+
+        //then
+        assertEquals(membersChat.size(), 10);
+    }
+
+    @DisplayName("회원의 게시물들을 반환한다.")
+    @Test
+    public void find_members_boards() {
+        //given
+        Member member = initializeMember();
+
+        //when
+        IntStream.range(0, 10)
+                .forEach(id -> boardRepository.save(Board.of("title" + id, "content" + id, member)));
+
+        //then
+        assertAll(
+                () -> assertEquals(memberService.findMembersBoard(member).stream()
+                                .map(BoardResponse::getTitle).collect(Collectors.toList()),
+                        List.of("title9", "title8", "title7", "title6", "title5", "title4", "title3", "title2",
+                                "title1", "title0")),
+
+                () -> assertEquals(memberService.findMembersBoard(member).stream()
+                                .map(BoardResponse::getContent).collect(Collectors.toList()),
+                        List.of("content9", "content8", "content7", "content6", "content5", "content4", "content3",
+                                "content2", "content1", "content0"))
+        );
+    }
+
+    @DisplayName("회원의 댓글을 반환한다.")
+    @Test
+    void find_members_comments() {
+        // given
+        Member member = initializeMember();
+        Board board = boardRepository.save(Board.of("title", "content", member));
+
+        // when
+        IntStream.range(0, 10)
+                .forEach(each -> commentRepository.save(BoardComment.of("content" + each, member, board)));
+
+        // then
+        List<MembersCommentResponse> membersCommentResponses = memberService.findMembersComment(member);
+        assertAll(
+                () -> IntStream.range(0, 10).forEach(index -> {
+                    assertEquals("content" + (9 - index), membersCommentResponses.get(index).getContent());
+                    assertEquals(member.getEmail(), membersCommentResponses.get(index).getEmail());
+                    assertEquals(board.getId(), membersCommentResponses.get(index).getBoardId());
+                })
+        );
+    }
+
+    private Member initializeMember() {
+        Member member = createMember();
+        memberRepository.save(member);
+        return memberRepository.findByEmail(member.getEmail()).get();
     }
 }

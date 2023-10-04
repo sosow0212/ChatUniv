1aeabe7ff5bd141e91dfafb82125ed8a99e727e7
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-10-04 20:01:11 +0900

    feat: 채팅방 검색 기능 추가 (#76)
    * refactor: 커멘드와 쿼리를 분리
    
    * feat: 조회를 위한 DTO 추가
    
    * test: 채팅방 검색 테스트 추가
    
    * docs: 채팅방 검색에 대한 문서화 추가
    
    * refactor: 불필요한 개행 삭제
    
    * refactor: 검색어가 없을 경우 에러 메시지 추가
    
    * refactor: 페이징 처리 추가
    
    * test: 페이징 처리로 인한 테스트 변경
    
    * test: docs를 위한 requestParameters 추가
    
    * docs: 검색에 parameter 상세 정보 추가

--- src/test/java/mju/chatuniv/chat/service/ChatServiceIntegrationTest.java
+++ src/test/java/mju/chatuniv/chat/service/ChatServiceIntegrationTest.java
@@ -1,10 +1,14 @@
 package mju.chatuniv.chat.service;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
 import mju.chatuniv.chat.domain.chat.Chat;
 import mju.chatuniv.chat.domain.chat.ChatRepository;
 import mju.chatuniv.chat.domain.chat.Conversation;
 import mju.chatuniv.chat.domain.chat.ConversationRepository;
 import mju.chatuniv.chat.domain.word.WordRepository;
+import mju.chatuniv.chat.infrastructure.dto.ConversationSimpleResponse;
 import mju.chatuniv.chat.service.dto.chat.ChattingHistoryResponse;
 import mju.chatuniv.helper.integration.IntegrationTest;
 import mju.chatuniv.member.domain.Member;
@@ -13,12 +17,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 public class ChatServiceIntegrationTest extends IntegrationTest {
 
     @Autowired
     private ChatService chatService;
+
+    @Autowired
+    private ChatQueryService chatQueryService;
 
     @Autowired
     private WordRepository wordRepository;
@@ -55,9 +60,28 @@ public class ChatServiceIntegrationTest extends IntegrationTest {
         conversationRepository.save(Conversation.of("ask2", "answer2", chat));
 
         // when
-        ChattingHistoryResponse result = chatService.joinChattingRoom(chat.getId(), member);
+        ChattingHistoryResponse result = chatQueryService.joinChattingRoom(chat.getId(), member);
 
         // then
         assertThat(result.getConversations().size()).isEqualTo(2);
+    }
+
+    @DisplayName("채팅방을 검색한다.")
+    @Test
+    void search_chatting_room() {
+        // given
+        Member member = memberRepository.save(Member.of("a@a.com", "password"));
+        Chat chat = chatRepository.save(Chat.createDefault(member));
+        conversationRepository.save(Conversation.of("i love spring", "answer", chat));
+        conversationRepository.save(Conversation.of("ask", "answer2", chat));
+        String keyword = "love";
+        Integer pageSize = 10;
+        Long conversationId = 3L;
+
+        // when
+        List<ConversationSimpleResponse> result = chatQueryService.searchChattingRoom(keyword,pageSize,conversationId);
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
     }
 }

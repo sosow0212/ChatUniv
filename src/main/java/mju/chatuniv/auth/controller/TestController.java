a0676cb1255399eccb71a2331b2c5c5b961b37f5
sosow0212 <sosow0212@naver.com>
2023-10-07 20:22:07 +0900

    feat: 테스트용 클래스 생성

---
+++ src/main/java/mju/chatuniv/auth/controller/TestController.java
@@ -0,0 +1,14 @@
+package mju.chatuniv.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
+}

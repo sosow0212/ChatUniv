5dada3fdef553d76629be2857862cfad5de12d3d
eom-tae-in <eti0728@daum.net>
2023-10-09 22:35:25 +0900

    refactor: 테스트용 데이터베이스에 맞게 truncate 쿼리 변경
    Co-authored-by: kimtaesoo99 <kimtaesoo7@naver.com>

--- src/test/java/mju/chatuniv/helper/RepositoryTestHelper.java
+++ src/test/java/mju/chatuniv/helper/RepositoryTestHelper.java
@@ -4,14 +4,12 @@ import java.util.List;
 import mju.chatuniv.global.config.JpaAuditingConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Import(JpaAuditingConfig.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RepositoryTestHelper {
 
     @Autowired
@@ -20,18 +18,18 @@ public class RepositoryTestHelper {
     @BeforeEach
     void initDatabase() {
         List<String> truncateAllTablesQuery = jdbcTemplate.queryForList(
-                "SELECT CONCAT('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'",
+                "SELECT CONCAT('TRUNCATE TABLE ', TABLE_NAME, ' RESTART IDENTITY', ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'",
                 String.class);
         truncateAllTables(truncateAllTablesQuery);
     }
 
     private void truncateAllTables(final List<String> truncateAllTablesQuery) {
-        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
 
         for (String truncateQuery : truncateAllTablesQuery) {
             jdbcTemplate.execute(truncateQuery);
         }
 
-        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
     }
 }

c22ca68dac53e3283d748738836c666a62dc97f3
kimtaesoo <kimtaesoo7@naver.com>
2023-10-10 01:21:21 +0900

    refactor: 게시판의 작성자와 댓글의 작성자를 리턴하도록 수정

--- src/main/java/mju/chatuniv/comment/infrastructure/repository/dto/CommentPagingResponse.java
+++ src/main/java/mju/chatuniv/comment/infrastructure/repository/dto/CommentPagingResponse.java
@@ -1,16 +1,24 @@
 package mju.chatuniv.comment.infrastructure.repository.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import java.time.LocalDateTime;
 
 public class CommentPagingResponse {
 
     private Long commentId;
     private String content;
+    private String email;
+    private LocalDateTime createAt;
 
     @QueryProjection
-    public CommentPagingResponse(final Long commentId, final String content) {
+    public CommentPagingResponse(final Long commentId,
+                                 final String content,
+                                 final String email,
+                                 final LocalDateTime createAt) {
         this.commentId = commentId;
         this.content = content;
+        this.email = email;
+        this.createAt = createAt;
     }
 
     private CommentPagingResponse() {
@@ -22,5 +30,13 @@ public class CommentPagingResponse {
 
     public String getContent() {
         return content;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
     }
 }

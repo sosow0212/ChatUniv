3005b67a97bab2ae656f40a0c699ddd289304115
eom-tae-in <eti0728@daum.net>
2023-12-01 13:54:29 +0900

    refactor: 응답 헤더 설정 변경
    Access-Control-Expose-Headers 설정 추가

--- src/main/java/mju/chatuniv/global/config/CorsFilter.java
+++ src/main/java/mju/chatuniv/global/config/CorsFilter.java
@@ -17,6 +17,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Expose-Headers", "*");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);

519015984c50c6619ebf08ccb6633a506a5f6478
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-12-09 18:23:19 +0900

    refactor: 모든 출처에서 요청을 허용하도록 수정 (#105)

--- src/main/java/mju/chatuniv/global/config/CorsFilter.java
+++ src/main/java/mju/chatuniv/global/config/CorsFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CorsFilter extends OncePerRequestFilter {
@@ -12,7 +13,7 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
                                     final FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
